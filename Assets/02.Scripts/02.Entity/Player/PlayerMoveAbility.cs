aacf70ff8d227d0ffff9127f878a2ff9bf7de4b9
PSH <packtty18@naver.com>
2026-02-23 15:36:09 +0900

    Feat : 플레이어 이동 및 점프

--- Assets/02.Scripts/02.Entity/Player/PlayerMoveAbility.cs
+++ Assets/02.Scripts/02.Entity/Player/PlayerMoveAbility.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class PlayerMoveAbility : MonoBehaviour
 {
-    //임시
-    public float MoveSpeed = 7f;
+    private float _gravity = -9f;
+    
     private CharacterController _characterController;
+    
+    //스텟
+    public float MoveSpeed = 7f;
+    public float JumpPower = 2.5f;
+    
+    //누적 y
+    private float _yVelocity = 0f;
 
     private void Start()
     {
@@ -13,18 +20,21 @@ public class PlayerMoveAbility : MonoBehaviour
     
     private void Update()
     {
-        // 목표: 키보드 [W], [A], [S], [D] 키를 누르면 캐릭터를 그 방향으로 이동시키고 싶다.
-        // 순서:
-        // 1. 사용자의 키보드 입력 받기
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        // 2. '캐릭터가 바라보는 방향'을 기준으로 방향 설정하기
         Vector3 dir = new Vector3(h, 0, v);
-        dir = dir.normalized; // = dir.Normalize();
+        dir = dir.normalized;
 		
-        // 3. 이동 속도에 따라 그 방향으로 이동하기
-        // 캐릭터의 위치 = 현재 위치 + 속도  * 시간
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            Debug.Log("점프");
+            _yVelocity = JumpPower;
+        }
+        
+        _yVelocity += _gravity * Time.deltaTime;
+        dir.y = _yVelocity;
+
         _characterController.Move(dir * MoveSpeed * Time.deltaTime);
     }
 }
