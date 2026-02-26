7ceca1f293e961e11305f220e1426d43b29eb9b2
PSH <packtty18@naver.com>
2026-02-26 12:52:04 +0900

    Refactor : 아이템 오브젝트 변경

--- Assets/02.Scripts/02.InGame/02.Item/ScoreItem.cs
+++ Assets/02.Scripts/02.InGame/02.Item/ScoreItem.cs
@@ -1,5 +1,6 @@
 using System;
 using Photon.Pun;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ScoreItem : MonoBehaviourPun
@@ -8,6 +9,7 @@ public class ScoreItem : MonoBehaviourPun
     {
         if (other.TryGetComponent( out PlayerController player) && !player.IsDead)
         {
+            Debug.Log("아이템 획득");
             player.Score += 100;
             ItemObjectFactory.Instance.RequestDelete(photonView.ViewID);
         }
