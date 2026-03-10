6ff3a1170753bdfa3e227a39ac9902ed43f18fee
PSH <packtty18@naver.com>
2026-03-10 14:40:22 +0900

    Fix : 플레이어가 레코더를 찾지 못하는 현상 제거

--- Assets/02.Scripts/02.InGame/02.Entity/Player/Abilities/PlayerVoiceAbility.cs
+++ Assets/02.Scripts/02.InGame/02.Entity/Player/Abilities/PlayerVoiceAbility.cs
@@ -1,40 +1,83 @@
-using System;
+using Photon.Pun;
 using Photon.Voice.PUN;
 using Photon.Voice.Unity;
 using UnityEngine;
 
 public class PlayerVoiceAbility : PlayerAbility
 {
-    [SerializeField]  private GameObject _speakingIcon;
+    [SerializeField] private GameObject _speakingIcon;
     [SerializeField] private PhotonVoiceView _voiceView;
+
     private Recorder _recorder;
 
     private void Start()
     {
-        _recorder = FindAnyObjectByType<Recorder>();
-
-        _recorder.VoiceDetection = true;
-        _recorder.VoiceDetectionThreshold = 0.01f;
-        _recorder.VoiceDetectionDelayMs = 300;
+        InitializeRecorder();
     }
 
     private void Update()
     {
+        if (_recorder == null)
+        {
+            InitializeRecorder();
+        }
+
         bool isSpeaking = false;
+
         if (_owner.PhotonView.IsMine)
         {
-            if(Input.GetKeyDown(KeyCode.M))
+            if (_recorder != null)
             {
-                _recorder.TransmitEnabled = !_recorder.TransmitEnabled;
+                if (Input.GetKeyDown(KeyCode.M))
+                {
+                    _recorder.TransmitEnabled = !_recorder.TransmitEnabled;
+                    Debug.Log($"TransmitEnabled : {_recorder.TransmitEnabled}");
+                }
+
+                isSpeaking = _recorder.TransmitEnabled && _recorder.IsCurrentlyTransmitting;
             }
-            isSpeaking = _recorder.IsCurrentlyTransmitting;
         }
         else
         {
-            isSpeaking = _voiceView.IsSpeaking;
+            if (_voiceView != null)
+            {
+                isSpeaking = _voiceView.IsSpeaking;
+            }
         }
-        
-        _speakingIcon.gameObject.SetActive(isSpeaking);
-        
+
+        if (_speakingIcon != null)
+        {
+            _speakingIcon.SetActive(isSpeaking);
+        }
+    }
+
+    private void InitializeRecorder()
+    {
+        if (_recorder != null)
+        {
+            return;
+        }
+
+        if (_voiceView != null && _voiceView.RecorderInUse != null)
+        {
+            _recorder = _voiceView.RecorderInUse;
+        }
+
+        if (_recorder == null)
+        {
+            _recorder = FindAnyObjectByType<Recorder>();
+        }
+
+        if (_recorder == null)
+        {
+            Debug.LogWarning("[PlayerVoiceAbility] Recorder를 찾지 못했습니다.");
+            return;
+        }
+
+        _recorder.VoiceDetection = true;
+        _recorder.VoiceDetectionThreshold = 0.01f;
+        _recorder.VoiceDetectionDelayMs = 300;
+
+        Debug.Log($"[PlayerVoiceAbility] Recorder 찾음: {_recorder.name}");
     }
 }
